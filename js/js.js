ee5806716aa7837f50d95813006418c7e60a3fd3
Kate <ekaty.linc0@gmail.com>
2022-03-13 16:54:31 +0300

    js делали переход цветов

--- js/js.js
+++ js/js.js
@@ -2,9 +2,26 @@ let red = 0
 let green = 0
 let blue = 0
 setInterval(() => {
-document.querySelector('html').style.background=`rgb(${red},${green},${blue})`
-red++
-blue++
- green++
-})
+    if (red == 0) {
+        // console.log(new Date().getMilliseconds())
+        for (let i = 0; i < 256; i++) {
+            // console.log(1)
+            setInterval(() => {
+                red++
+                document.querySelector('html').style.background = `rgb(${red},${green},${blue})`
+            }, i * 100);
+        }
+        // console.log(new Date().getMilliseconds())
 
+    }
+    else if (red >= 255){
+        for (let i = 0; i < 256; i++) {
+            // console.log(2)
+            setInterval(() => {
+                red--
+                document.querySelector('html').style.background = `rgb(${red},${green},${blue})`
+            }, i * 100);
+        }
+    }
+
+}, 3500)
